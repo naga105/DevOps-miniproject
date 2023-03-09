01290bc14d8f95793d0318bd5c62be1add0e2a8f
root <root@ip-172-31-45-13.ap-southeast-1.compute.internal>
2023-03-09 04:48:23 +0000

    push terraform aws

---
+++ terraform-aws-construct/variables.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "access_key_id" {
+  description = "aws access key"
+  type        = string
+  default     = "AKIAS3ISQ7SWUDYS2CHK"
+}
+
+variable "secret_access_key" {
+  description = "aws access key"
+  type        = string
+  default     = "PpmPDL84mKOwdwO1+/V9d5pep5E9k65CdthdfvZ2"
+}
+
